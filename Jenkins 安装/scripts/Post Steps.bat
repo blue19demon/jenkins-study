6e3093728a196debf25606d0fd48a68c3d07a958
zhangbo <363230482@qq.com>
2019-07-11 10:57:20 +0800

    modify

--- Jenkins 安装/scripts/Post Steps.bat
+++ Jenkins 安装/scripts/Post Steps.bat
@@ -1,5 +1,11 @@
 @echo off
 set current_path="%cd%"
-echo µ±Ç°Â·¾¶ = %current_path%
-start javaw -jar %current_path%\%project_name%\target\%project_name%-0.0.1-SNAPSHOT.jar
+echo maven package finished
+echo begin create %project_name% war's %env% environment Folder
+md %current_path%\deploy\%env%\war
+echo begin copy war
+copy %current_path%\%project_name%\target\%project_name%-0.0.1-SNAPSHOT.jar %current_path%\deploy\%env%\war\%project_name%-0.0.1-SNAPSHOT.jar
+echo begin deploy project
+cd %current_path%\deploy\%env%\war
+start javaw -jar %project_name%-0.0.1-SNAPSHOT.jar
 exit
